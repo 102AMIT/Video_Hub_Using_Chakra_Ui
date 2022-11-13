7ef2bb9e9016f9a6ff1ace52225f8769aaffbd31
Amit Kumar Thakur <kingston.amit9830@gmail.com>
2022-11-14 02:33:52 +0530

    upload section completed

--- src/App.js
+++ src/App.js
@@ -3,6 +3,11 @@ import Header from "./components/Header"
 import Home from "./components/Home"
 import Footer from "./components/Footer"
 import Videos from "./components/Videos"
+import Upload from "./components/Upload"
+import Login from "./components/Login"
+import Signup from "./components/Signup"
+
+
 
 function App() {
   return (
@@ -11,6 +16,10 @@ function App() {
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/videos' element={<Videos />} />
+        <Route path='/upload' element={<Upload />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/signup' element={<Signup />} />
+        
       </Routes>
       <Footer />
     </Router>
